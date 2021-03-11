f6a33b607e75ca7d46cb015639ce2d2c1c75d6d3
Willian Mazzorana <willianmazzorana@hotmail.com>
2021-03-10 22:01:13 -0300

    Criado a listagem de autores

--- src/app/autores/autores.page.ts
+++ src/app/autores/autores.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Autor } from './autor.model';
+import { Genero } from './genero.enum';
 
 @Component({
   selector: 'app-autores',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AutoresPage implements OnInit {
 
-  constructor() { }
+  autores: Autor[];
+
+  constructor() { 
+    this.autores = [
+      { nome: 'David Flanagen',   dataNascimento: new Date(1980,11,12), genero: Genero.MASCULINO },
+      { nome: 'Douglas Cockford', dataNascimento: new Date(1975,9,23), genero: Genero.MASCULINO },
+      { nome: 'Martin Flower',    dataNascimento: new Date(1956,12,5), genero: Genero.MASCULINO }
+    ];
+  }
 
   ngOnInit() {
   }
